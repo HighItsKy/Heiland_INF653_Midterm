c362bac9bb9022a2efee4430d4a88a199e703938
HighItsKy <73318996+HighItsKy@users.noreply.github.com>
2023-03-16 13:55:08 -0500

    The coors didn't need adjusting; reverted back.

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -1,8 +1,8 @@
 <?php
-//Header
+/*//Header
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
+*/
 include_once '../../config/Database.php';
 include_once '../../models/Author.php';
 
